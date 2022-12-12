7f0a0432bbc039c75a99132a9e73a47557f3650a
rhuemer15 <120364894+rhuemer15@users.noreply.github.com>
2022-12-12 10:05:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = File.ReadAllLines("@Z:\\5. Klasse\\SWP\\AoC2022\\AoC2022\\aoc2022-day01-input.txt");
+            Console.WriteLine(input);
+        }
+    }
+}
